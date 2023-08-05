724846eb7a1eaf39976b70f5e6c12b00486ebf3b
Yozora09 <yozoraizumi@gmail.com>
2023-08-05 11:42:02 +0800

    Onboard swipers added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { Page1Component } from '../components/onboard-pages/page1/page1.component';
+import { Page2Component } from '../components/onboard-pages/page2/page2.component';
+import { Page3Component } from '../components/onboard-pages/page3/page3.component';
 
 
 @NgModule({
@@ -14,6 +18,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, Page1Component, Page2Component, Page3Component],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
